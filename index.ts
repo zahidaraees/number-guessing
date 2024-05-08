ff4ead01c5e05ab472c48de5f6d11cd44edd3945
zahidaraees <160402259+zahidaraees@users.noreply.github.com>
2024-05-08 05:24:35 -0700

    commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /user/bin/env  node
+//Number Guessing Applicaiton  
+
+import inquirer from "inquirer";
+let roundNumber: number = Math.floor(Math.random() * 4 + 1);
+
+let userInput = await inquirer.prompt({
+  type: "number",
+  name: "guessNumber",
+  message: "Guess a Number between 1 to 4 :",
+});
+
+if (userInput.guessNumber === roundNumber) {
+  console.log("Congratulations! you have guessed correctly!");
+} else {
+  console.log(
+    `Sorry! your answer is wrong, plz try again because at this time correct ans is: ${roundNumber}`
+  );
+}
